0525ddba3e0f6c304567b40f7bad23e55dffa3ce
Aziz Batihk <aziz@azizmb.com>
2015-07-25 21:26:42 -0400

    Created KeyStore class.

---
+++ Bullbucks/KeyStore.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Foundation;
+
+namespace Bullbucks
+{
+	public static class KeyStore
+	{
+		private static NSUserDefaults _Defaults = new NSUserDefaults("group.com.azizmb.bullbucks", NSUserDefaultsType.SuiteName);
+
+		public static string FirstName
+		{
+			get {
+				_Defaults.StringForKey ("FirstName");
+			}
+
+			set {
+				_Defaults.SetString (value, "FirstName");
+			}
+		}
+
+		public static string LastName
+		{
+			get {
+				_Defaults.StringForKey ("LastName");
+			}
+
+			set {
+				_Defaults.SetString (value, "LastName");
+			}
+		}
+
+		public static string UID
+		{
+			get {
+				_Defaults.StringForKey ("UID");
+			}
+
+			set {
+				_Defaults.SetString (value, "UID");
+			}
+		}
+
+		public static string CardNumber
+		{
+			get {
+				_Defaults.StringForKey ("CardNumber");
+			}
+
+			set {
+				_Defaults.SetString (value, "CardNumber");
+			}
+		}
+	}
+}
+
